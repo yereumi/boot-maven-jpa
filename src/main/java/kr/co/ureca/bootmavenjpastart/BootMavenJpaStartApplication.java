68359ee05bc3b6c3de8f7efa0d688966e0851769
yereumi <lovelyyeji01@gmail.com>
2025-03-31 17:54:41 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/kr/co/ureca/bootmavenjpastart/BootMavenJpaStartApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ureca.bootmavenjpastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMavenJpaStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMavenJpaStartApplication.class, args);
+    }
+
+}
